8f47c66bd46c6f996a67cd9a6560e010f8992adb
stephanieasitimbay02 <stephanieasitimbay02@gmail.com>
2025-12-09 16:46:56 -0500

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -6,9 +6,9 @@
 //
 
 #include <iostream>
-
+using namespace std;
 int main() {
-    // insert code here...
-    std::cout << "Hello, World!" << std::endl;
+    
+    cout << "Hello, World!" << endl;
     return 0;
 }
